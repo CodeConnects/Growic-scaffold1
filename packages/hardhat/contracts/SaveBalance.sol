85ebc2aa0fd70d534964b79cf2d1c04050941258
oca <codeconnects@gmail.com>
2022-12-28 14:05:41 -0500

    save balance contract initial structure

---
+++ packages/hardhat/contracts/SaveBalance.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+
+contract SaveBalance {
+    string public purpose = "Save a user balance";
+
+    constructor() payable {
+    }
+}
