8fd5fd58d1ea736c388068be60daaa088b39f5cc
oca <codeconnects@gmail.com>
2022-12-27 21:50:35 -0500

    add transfer ownership

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -39,6 +39,11 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     //const YourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   */
 
+  await YourContract.transferOwnership(
+    "0x6288A9F9eFcd588DBBFBF456838071B97f31223F"
+  );
+
+
   /*
   //If you want to send value to an address from the deployer
   const deployerWallet = ethers.provider.getSigner()
