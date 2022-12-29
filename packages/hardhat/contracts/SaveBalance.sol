681add3eb94a5efbee281a9d6a34bbe7c7dc1208
oca <codeconnects@gmail.com>
2022-12-29 12:35:41 -0500

    tidy up code

--- packages/hardhat/contracts/SaveBalance.sol
+++ packages/hardhat/contracts/SaveBalance.sol
@@ -2,23 +2,28 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol"; 
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract SaveBalance {
+    
     string public purpose = "Save a user balance";
 
     mapping(address => uint) public balances;
 
-    function deposit (uint256 amount) {
+    constructor() payable {
+    }
+
+    function deposit (uint256 amount) public {
         // save amount deposited into mapping of address => balance
         balances[msg.sender] += amount;
     }
 
     function checkBalance() public view returns (uint) {
-        // search for user balance inside the balance mapping
         // return the balance of address calling the contract (msg.sender)
         return balances[msg.sender];
     }
 
-    constructor() payable {
-    }
+    receive() external payable {}
+    fallback() external payable {}
 }
