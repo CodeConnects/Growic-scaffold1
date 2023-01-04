4421751c542a6f872d84f6b8b188744f0226730e
oca <codeconnects@gmail.com>
2023-01-04 09:52:15 -0500

    add base of userDetails

--- packages/hardhat/contracts/Structs.sol
+++ packages/hardhat/contracts/Structs.sol
@@ -9,20 +9,35 @@ contract Structs {
     
     string public purpose = "Save user balance and KYC details";
 
+    // Mappings section
     mapping(address => uint) public balances;
 
-    constructor() payable {
+    // Structs section
+    struct UserDetail {
+        string name;
+        uint256 age;
+    }
+    mapping(address => UserDetail) public userDetails;
+
+
+
+/********************* Structs section *********************/
+
+    // save details of user calling smart contract
+    // save them into a defined struct
+    function setUserDetails(string calldata _name, uint256 _age) {
+        userDetails[msg.sender].name = _name;
+        userDetails[msg.sender].age = _age;
     }
 
-    // setUserDetails(string calldata name, uint256 age) 
-    // this function accepts 2 arguments
-    // these represent the details of user calling smart contract
-    // it saves them into a defined struct
 
-
-    // getUserDetail() 
     // retrieves and returns the details saved for the user calling the contract.
+    function getUserDetail() public view returns (string, uint256) {
+        // return the saved details of the user calling smart contract
+        return userDetails[msg.sender];
+    }
 
+/********************* Mappings section functions *********************/
 
     function deposit (uint256 amount) public {
         // save amount deposited into mapping of address => balance
@@ -34,6 +49,10 @@ contract Structs {
         return balances[msg.sender];
     }
 
+
+
+    constructor() payable {
+    }
     receive() external payable {}
     fallback() external payable {}
 }
